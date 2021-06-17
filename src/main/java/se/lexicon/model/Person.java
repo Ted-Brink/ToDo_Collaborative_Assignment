3aa5c2696e3378a991925beb0c606480f82ac474
Ted-Brink <hexasus@gmail.com>
2021-06-17 11:00:06 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/model/Person.java
@@ -0,0 +1,38 @@
+package se.lexicon.model;
+
+
+class Person {
+    final int personId;
+    String firstName;
+    String lastName;
+
+    Person(final int personId, String firstName, String lastName) {
+      this.personId = personId;
+      this.firstName = firstName;
+      this.lastName = lastName;
+      }
+      public int getPersonID() {
+        return personId;
+    }
+    public void setPersonID(int personID) {
+       // this.personId = personId;               // Tror inte det ska var någon metod här eftersom personId är final
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
+
+
