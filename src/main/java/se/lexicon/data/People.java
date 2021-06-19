65246cc464a66257213f9f489d55a40e89c225ef
Ted-Brink <hexasus@gmail.com>
2021-06-19 14:36:02 +0200

    update

--- src/main/java/se/lexicon/data/People.java
+++ src/main/java/se/lexicon/data/People.java
@@ -7,25 +7,14 @@ import java.util.Arrays;
 class People {
    private static Person[] personer = new Person[0];   // Array för lagring av Person
 
-   public int size() {
+   public int size() {                                 // Antal moduler i Arrayen
        return personer.length;
    }
 
-   public Person[] findAll() {
+   public Person[] findAll() {                          // Returnerar en Array med alla Personobjekt.
        Person[] personLista = Arrays.copyOf(personer,personer.length);
        return personLista;
    }
-
-
-
-
-/*
-    public static String[] findAllTitles(){
-        String[] songList = Arrays.copyOf(songTitles, songTitles.length);
-        return songList;
-    }
-
-        */
 
 
     // public Person findById(int personId) {}  //////// GÖR METOD
@@ -33,7 +22,7 @@ class People {
 
     // Metod skapa ny Person
 
-    public static boolean skapaNyPerson(Person nyPerson) { // behöver det vara en boolean retur???
+    public static boolean skapaNyPerson(Person nyPerson) {              // behöver det vara en boolean retur???
 
         // Utökning av Array
         Person[] expandedArray = Arrays.copyOf(personer, personer.length + 1);
@@ -49,21 +38,15 @@ class People {
         System.out.println("Person tilllagd");
         //   System.out.println("Arrays.toString(personer) = " + Arrays.toString(songTitles)); Har ingen toStringmetod för Person-objekt
         return true;
-
     }
-
 
     public void clearPersoner(){              // static??? clear Person Array
        personer = new Person[0];
     }
 
-
-
     // När allt annat är klart:
     // Lägg till Functionality to remove object from array. (not nulling) First: you need to find the correct array
     // index of the object. Second: You need to rebuild array by excluding the object on found index.
-
-
 }
 
 
